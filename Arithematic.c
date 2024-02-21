4db5a8883a1afe8e9fc3ceb0d43c64dede20078f
Angel Theresa Paul <angeltheresa2009@gmail.com>
2024-02-21 20:05:10 +0530

    Arithematic.c

---
+++ Arithematic.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("Enter number one\n");
+    scanf("%d",&a);
+    printf("Enter number two");
+    scanf("%d",&b);
+    printf("Addition of %d and %d=%d\n",a,b,a+b);
+    printf("Subtraction of %d and %d=%d\n",a,b,a-b);
+    printf("Division of %d and %d=%d\n",a,b,a/b);
+    printf("Multiplication of %d and %d=%d\n",a,b,a*b);
+    printf("Remainder of %d and %d=%d\n",a,b,a%b);
+    return 0;
+}
