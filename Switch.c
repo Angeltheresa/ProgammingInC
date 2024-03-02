2b69f89c21c87b14a6b8076372958e89b63cb54d
Angel Theresa Paul <angeltheresa2009@gmail.com>
2024-03-02 11:35:07 +0530

    Switch.c

---
+++ Switch.c
@@ -0,0 +1,93 @@
+/*days of the week*/
+#include<stdio.h>
+int main() 
+{
+    int day;
+    printf("Enter the day number: ");
+    scanf("%d", &day);
+
+    switch(day)
+    {
+        case 1:
+            printf("Monday\n");
+            break;
+        case 2:
+            printf("Tuesday\n");
+            break;
+        case 3:
+            printf("Wednesday\n");
+            break;
+        case 4:
+            printf("Thursday\n");
+            break;
+        case 5:
+            printf("Friday\n");
+            break;
+        case 6:
+            printf("Saturday\n");
+            break;
+        case 7:
+            printf("Sunday\n");
+            break;
+        default:
+            printf("Invalid day number\n");
+    }
+
+    return 0;
+}
+
+
+
+
+/*student marklist*/
+#include<stdio.h>
+
+int main()
+{
+    char name[50];
+    int regnum, sem, sub1, sub2, sub3, total;
+    float average, marks;
+
+    printf("Name: ");
+    scanf("%s", name);
+    printf("Register number: ");
+    scanf("%d", &regnum);
+    printf("Semester: ");
+    scanf("%d", &sem);
+    printf("Enter the marks: ");
+    scanf("%d %d %d", &sub1, &sub2, &sub3);
+
+    total = sub1 + sub2 + sub3;
+    average = total / 3.0; // calculate average as a floating-point value
+
+    if (average > 90) 
+    {
+        printf("Grade 'O'\n");
+    } 
+    else if (average > 85)
+    {
+        printf("Grade 'A'\n");
+    } 
+    else if (average > 80) 
+    {
+        printf("Grade 'B'\n");
+    } 
+    else if (average > 70)
+    {
+        printf("Grade 'C'\n");
+    } 
+    else if (average > 50) 
+    {
+        printf("Grade 'D'\n");
+    } 
+    else 
+    {
+        printf("Grade 'F'\n");
+    }
+
+    printf("Total Marks: %d\n", total);
+    printf("Average Marks: %f\n", average);
+
+    return 0;
+}
+
