b5c7cba69a63e73cb63facb1c9e8938638298e09
Angel Theresa Paul <angeltheresa2009@gmail.com>
2024-03-13 22:08:17 +0530

    PrimeOrNot(For).c

---
+++ PrimeOrNot(For).c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+void main() {
+    int num, i, isPrime = 1;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    
+    for (i = 2; i <= num / 2; i++) {
+        if (num % i == 0) {
+            isPrime = 0;
+            break;
+        }
+    }
+
+    if (isPrime) {
+        printf("%d is a prime number.\n", num);
+    } else {
+        printf("%d is not a prime number.\n", num);
+    }
+}
